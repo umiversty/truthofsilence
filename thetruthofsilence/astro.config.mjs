b878ecb48bfcb87b66c4134d8535bb8e8a42fdb8
ckellc <baker.coleman@gmail.com>
2025-11-16 22:38:00 -0700

    ff

--- thetruthofsilence/astro.config.mjs
+++ thetruthofsilence/astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://thetruthofsilence.com",
+	site: "https://thetruthofsilence.org",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
